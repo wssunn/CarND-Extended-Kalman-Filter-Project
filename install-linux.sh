ab60378fa167211619e82722ff108ba8df86424c
ning <ningsunprc@gmail.com>
2019-08-20 15:05:28 +0800

    modified install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -10,9 +10,7 @@ else
 
   sudo apt-get update
   sudo apt-get install git libuv1-dev libssl-dev gcc g++ cmake make
-fi
-
-git clone https://github.com/uWebSockets/uWebSockets 
+fi 
 cd uWebSockets
 git checkout e94b6e1
 mkdir build
@@ -22,4 +20,3 @@ make
 sudo make install
 cd ../..
 sudo ln -s /usr/lib64/libuWS.so /usr/lib/libuWS.so
-sudo rm -r uWebSockets
